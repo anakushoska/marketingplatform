dae4ecf6936f430a3f62d91c43ccfb4686ea0bb4
Ana Kushoska <akushoska@gmail.com>
2020-07-22 15:04:18 +0200

    add routes file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Notification;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard.layouts.admin');
 });
+
+Route::post('/file-upload', 'Dashboard\NotificationController@fileupload')->name('upload.photo');
+
+Route::get('notifications','Dashboard\NotificationController@index')->name('notifications.index');
+Route::get('notifications/create', 'Dashboard\NotificationController@create')->name("notifications.create");
+Route::post('notifications', 'Dashboard\NotificationController@store')->name("notifications.store");
+Route::get('patients/{notification_id}/edit', 'Dashboard\NotificationController@edit')->name("notifications.edit");
+Route::put('patients/{notification_id}', 'Dashboard\NotificationController@update')->name("notifications.update");
+Route::delete('patients/{notification_id}/delete', 'Dashboard\NotificationController@destroy')->name("notifications.destroy");
+
+
+
+
+Route::get('dashboard', 'Dashboard\DashboardController@index')->name('dashboard');
+
+
+Route::get('test/1', function () {
+    $notification=new Notification;
+    $notification->subject='da vidime dali kje raboti';
+    $notification->body='Treba da smislam nekoj mnogogogogoiuouygo dolg zbor';
+    $notification->status=false;
+    $notification->number_of_emails=5;
+    $notification->save();
+
+    return $notification->getWordWithMostWovels();
+}
+
+
+
+);
